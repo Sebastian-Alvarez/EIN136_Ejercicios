86f3e50dee517df98a2e6202259f911331bd348f
Sebastian-Alvarez <sebastian.alvarezav@usm.cl>
2023-10-04 13:36:34 -0300

    Exercise 1 (getcomponent method) - version changed

--- Clase_6/Assets/SoundMaker.cs
+++ Clase_6/Assets/SoundMaker.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class SoundMaker : MonoBehaviour
 {
-    public AudioSource audio1;
+    AudioSource auds1;
+    AudioSource auds2;
+    AudioSource auds3;
+
+    private void Start()
+    {
+        auds1 = GetComponent<AudioSource>();
+        auds2 = GetComponent<AudioSource>();
+        auds3 = GetComponent<AudioSource>();
+        
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            Debug.Log("Ola");
-            audio1.Play();
+            Debug.Log("played 1");
+            auds1.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Played 2");
+            auds2.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Played 3");
+            auds3.Play();
         }
     }
 }
